0e5dda067d7f8f3d69d163bf454b2e965a58aa66
sajeeb sarkar <86061041+sajeebsarkar69@users.noreply.github.com>
2021-10-23 20:54:24 +0600

    first

---
+++ bd.js
@@ -0,0 +1,35 @@
+function myFunction() {
+    var x = document.getElementById("menu");
+
+ if (x.style.display === "block")
+ 
+ {x.style.display ="none";}
+ 
+  else {
+      x.style.display ="block";
+  } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
